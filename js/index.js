2c5ecabe30c0d3521b3290cc45dd26c52b612bea
Kamil Grabczak <grabczakkamil@gmail.com>
2018-05-14 20:46:09 +0200

    zadanie zrobione - działa

---
+++ js/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function sprawdzCzyParzysta(x) {
+    
+if (x % 2 == 0) {
+    return true;
+}    
+else {
+    return false;
+}    
+    
+};
+
+console.log(sprawdzCzyParzysta(4));
+console.log(sprawdzCzyParzysta(5));
+            
